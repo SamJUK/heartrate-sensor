1a469a959ac361f8c8d15e1e5036d54bb5293128
Sam <samjuk1999@gmail.com>
2023-05-29 21:04:52 +0100

    Added Argparse, device name & reformatted output of discover script

--- discover.py
+++ discover.py
@@ -1,21 +1,19 @@
 #/usr/bin/env python3
 
 import sys
+import argparse
 import asyncio
 import bleak
-
-def getDeviceId():
-    device_id = str(sys.argv[1])
-    if len(device_id) < 1:
-        print('[ERR]: No Device ID Presented')
-        sys.exit(1)
-    return device_id
 
 async def discover(addr):
     print('Discovering Address: {}'.format(addr))
 
-    async with bleak.BleakClient(addr, timeout=30) as client:
+    async with bleak.BleakClient(addr, timeout=30) as client: 
         print('> Connected.')
+
+        # Print Device Name
+        name = await client.read_gatt_char('00002a00-0000-1000-8000-00805f9b34fb')
+        print('> Device: {}'.format(name.decode('utf-8')))
 
         # List Services
         print('Services:')
@@ -23,8 +21,8 @@ async def discover(addr):
             svc = client.services.get_service(id)
             desc = svc.description
             uuid = svc.uuid
-            print('  [{}] {} {}'.format(uuid, id, desc))
-            print(','.join([str(chr.handle) for chr in svc.characteristics]))
+            char_ids = ','.join([str(chr.handle) for chr in svc.characteristics])
+            print('  [{}] {} {} ({})'.format(uuid, id, desc, char_ids))
 
         # List Characteristics
         print('\nCharacteristics: ')
@@ -32,9 +30,8 @@ async def discover(addr):
             chr = client.services.get_characteristic(id)
             desc = chr.description
             uuid = chr.uuid
-            print('  [{}] {} {}'.format(uuid, id, desc))
-            for property in chr.properties:
-                print('    - {}'.format(property))
+            properties_list = ','.join(chr.properties)
+            print('  [{}] {} {} ({})'.format(uuid, id, desc, properties_list))
 
         # List Descriptors
         print('\nDescriptors: ')
@@ -45,4 +42,8 @@ async def discover(addr):
             print('  [{}] {} {}'.format(uuid, id, des))
 
 
-asyncio.run(discover(getDeviceId()))
+parser = argparse.ArgumentParser(description='BLE Device Discovery')
+parser.add_argument('-d', '--device', required=True, help='The device ID to discover')
+config = parser.parse_args()
+
+asyncio.run(discover(config.device))
