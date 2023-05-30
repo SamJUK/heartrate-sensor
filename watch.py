4cf1e0429b2633f9ce85041a26348b30d8aa4be4
Sam James <sam@samdjames.uk>
2023-05-30 10:01:27 +0100

    Removed devices lookup, since the UUIDs should be universal (i think)

--- watch.py
+++ watch.py
@@ -1,11 +1,8 @@
 #/usr/bin/env python3
 
+from uuids import CHAR_DEVICE_NAME, CHAR_HR_MEASUREMENT
 import argparse
-import sys
 import asyncio
-import json
-from json.decoder import JSONDecodeError
-from time import sleep
 from datetime import datetime
 from bleak import BleakClient
 from bleak.exc import BleakError
@@ -15,30 +12,15 @@ monitoring = True
 config = None
 fh = None
 
-def getDeviceMapping(device):
-    try:
-        with open(f'devices/{device}.json', 'r') as data:
-            d = json.loads(data.read())
-            data.close()
-            return d
-    except JSONDecodeError:
-        print(f'[ERR]: Bad Config for device type {device}')
-    except FileNotFoundError:
-        print(f'[ERR]: Unknown Device {device}.')
-    
-    sys.exit()
-
 def parseArguments():
     global config
     parser = argparse.ArgumentParser(description='Heart Rate Monitor')
     parser.add_argument('-d', '--device', required=True, help='The device ID to monitor')
-    parser.add_argument('--device_type', default='garmin', help='The type of device')
     parser.add_argument('--file', action='store_true', help='Should write to file')
     parser.add_argument('--file_path', default='./out/hr.csv', help='Where should we write this file?')
     parser.add_argument('--keep_history', action='store_true', help='Keep HR History in output file')
     parser.add_argument('-v', '--verbose', action='store_true', help='Verbose Logging')
     config = parser.parse_args()
-    config.device_mapping = getDeviceMapping(config.device_type)
 
 def verbose(message):
     if config.verbose:
@@ -51,7 +33,7 @@ def setupFile():
         fh = open(config.file_path, mode)
 
 async def printDeviceName(client):
-    name = await client.read_gatt_char(config.device_mapping['CHAR_DEVICE_NAME'])
+    name = await client.read_gatt_char(CHAR_DEVICE_NAME)
     print('> Device: {}'.format(name.decode('utf-8')))
 
 def processFlags(flags):
@@ -83,21 +65,19 @@ def processHeartrate(sender, data):
         fh.flush()
 
 async def monitorHR(client):
-    CHAR_HR = config.device_mapping['CHAR_HR_MEASUREMENT']
-
-    verbose(f'Starting Notify for HR Measurement: {CHAR_HR}')
-    await client.start_notify(CHAR_HR, processHeartrate)
+    verbose(f'Starting Notify for HR Measurement: {CHAR_HR_MEASUREMENT}')
+    await client.start_notify(CHAR_HR_MEASUREMENT, processHeartrate)
 
     hasRecentUpdate = True
     while hasRecentUpdate:
         lastUpdateAgo = datetime.now().timestamp() - lastUpdate
         hasRecentUpdate = lastUpdateAgo < 10
         
-        verbose(f'Sleeping: {CHAR_HR}')
+        verbose(f'Sleeping: {CHAR_HR_MEASUREMENT}')
         await asyncio.sleep(5.0)
 
-    verbose(f'Stopping Notify for HR Measurement: {CHAR_HR}')
-    await client.stop_notify(CHAR_HR)
+    verbose(f'Stopping Notify for HR Measurement: {CHAR_HR_MEASUREMENT}')
+    await client.stop_notify(CHAR_HR_MEASUREMENT)
 
 
 async def main():
